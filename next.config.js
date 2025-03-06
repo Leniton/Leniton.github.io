2ee36f12a4c693571a110e5632d04b07a13335fb
Leniton <lenitoncarneiro@gmail.com>
2025-03-06 08:44:05 -0300

    update homepage

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    distDir: 'build',
+    basePath: '/build',
+    distDir: 'build/_next',
+    async redirects() {
+      return [
+        {
+          source: '/index.html',
+          destination: '/_next/server/app/index.html',
+          permanent: true,
+        },
+      ]
+    },
   }
